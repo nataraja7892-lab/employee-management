cad5c89e46acbe30e482f593efae4cb9b4c1514d
nataraja7892 <nataraja7892@gmail.com>
2025-10-15 17:10:41 +0530

    Initial commit

--- college_mgmt/settings.py
+++ college_mgmt/settings.py
@@ -123,7 +123,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_ROOT= BASE_DIR/'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
